2d87e97ff06f4980005b6d35c26ae24674d2cc12
Pham_Tim <phamt007@hsd.k12.or.us>
2019-03-13 09:27:22 -0700

    Initiate the git bash

---
+++ Assets/Script/PLayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizonal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizonal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
